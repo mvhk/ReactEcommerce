ea54e83d4a91d1592ff80f12c42075663a118a25
Venkata Harish Kumar Mamidi <mvhk@pm.me>
2020-08-16 23:11:39 +0530

    add Homepage UI hover effects and Navigation with withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,16 +13,19 @@ class Directory extends React.Component {
           title: 'Snacks',
           imageUrl: 'https://i.ibb.co/Gvkq8wH/snacks-in-america.jpg',
           id: 1,
+          linkUrl: 'hats',
         },
         {
           title: 'Personal Care',
           imageUrl: 'https://i.ibb.co/N1dtL9L/complexion-1576106034.jpg',
           id: 2,
+          linkUrl: '',
         },
         {
           title: 'Home Care',
           imageUrl: 'https://i.ibb.co/8PbCwhY/householdsupplies.jpg',
           id: 3,
+          linkUrl: '',
         },
         {
           title: 'Men',
@@ -30,6 +33,7 @@ class Directory extends React.Component {
             'https://i.ibb.co/8YyN8zj/2833225d9872042042168f86d4c8df99.jpg',
           id: 4,
           size: 'large',
+          linkUrl: '',
         },
         {
           title: 'Women',
@@ -37,6 +41,7 @@ class Directory extends React.Component {
             'https://i.ibb.co/s6KN3GQ/Set-of-fashion-accessories-Women-items-and-accessories-Vector-illustration.jpg',
           id: 5,
           size: 'large',
+          linkUrl: '',
         },
       ],
     };
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
